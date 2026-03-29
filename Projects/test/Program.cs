19b088803edc150f0c737808bbf49e717fb4d661
VoidSpaceTime <Space_Time_@outlook.com>
2026-03-29 19:10:08 +0800

    remove ecs

--- Projects/test/Program.cs
+++ Projects/test/Program.cs
@@ -43,19 +43,9 @@ namespace War3Frame
             if (isAot)
             {
                 var aot = new NativeAOT();
-                aot.RegisterComponent<Position>();
-                aot.RegisterComponent<Velocity>();
                 aot.CreateSchema();
             }
 
-            var world = new EntityStore();
-
-            // 使用 TimedSystemRoot
-            var root = new TimedSystemRoot(world);
-            root.SetMonitorPerf(true);
-
-            // 添加 System 并指定间隔：MoveSystem 每 1 秒更新一次
-            root.Add(new MoveSystem(), 1.0f);
 
             // 中心计时器
             var func = War3.GetNativeFunction("CreateTimer");
@@ -70,9 +60,7 @@ namespace War3Frame
             var unit = JassApi.CreateUnit(p0, JassApi.C2I("hfoo"), 0, 0, 270);
             // var unit2 = JassApi.CreateUnit(p0, JassApi.C2I("hfoo"), 0, 0, 270);
             Console.WriteLine($"创建单位 = {unit.Handle}");
-
-            var entity = world.CreateEntity(new Position(0, 0, 0),
-                new Velocity { value = new Vector3(0, 0, 0), unit = unit });
+            
 
             var tgr = JassApi.CreateTrigger();
             HandleHelper.HandleAdd(tgr);
@@ -88,31 +76,10 @@ namespace War3Frame
             
             War3.CallNative<int>(func, t, TICK_RATE, true, () =>
             {
-                root.Update(new(TICK_RATE, TimeSpan));
                 TimeSpan += TICK_RATE;
             });
         }
 
-        public struct Velocity : IComponent
-        {
-            public Vector3 value;
-            public JUnit unit;
-        }
-
-        // 使用原生 QuerySystem，频率由 TimedSystemRoot 控制
-        class MoveSystem : QuerySystem<Position>
-        {
-            protected override void OnUpdate()
-            {
-                Query.ForEachEntity((ref Position position, Entity entity) =>
-                {
-                    var unit = entity.GetComponent<Velocity>().unit;
-                    var ux = JassApi.GetUnitX(unit);
-                    var uy = JassApi.GetUnitY(unit);
-                    position.value = new Vector3(ux, uy, 0);
-                    Console.WriteLine($"更新单位位置: x: {ux}, y: {uy}");
-                });
-            }
-        }
+     
     }
 }
--- War3Frame/initialization/War3Init.cs
+++ War3Frame/initialization/War3Init.cs
@@ -10,24 +10,7 @@ namespace War3Frame
 
         public static void War3Init()
         {
-            // 初始化玩家关系管理器
-            PlayerNative[] players = new PlayerNative[16];
-            for (int i = 0; i < 16; i++)
-            {
-                var entity = Store.CreateEntity();
-                var player = JassApi.Player(i);
-                players[i] = new PlayerNative
-                {
-                    player = player,
-                    color = i,
-                    name = JassApi.GetPlayerName(player),
-                    index = i,
-                    getentity = entity,
-                };
-                entity.AddComponent<PlayerNative>(players[i]);
-            }
-            // 默认所有玩家全部相互敌对
-            PlayerHelper.InitializePlayers(ref players);
+    
         }
 
     }
